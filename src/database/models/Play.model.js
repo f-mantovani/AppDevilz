037061ecfc23ede4df69f4210b91ae7068886784
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-08-25 12:26:43 +0200

    created play.services

--- src/database/models/Play.model.js
+++ src/database/models/Play.model.js
@@ -1,22 +1,21 @@
 import mongoose from 'mongoose';
 
 const playSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        formation: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Formation',
-            },
-        ],
-        playImage: {
-            type: String,
-        },
-    },
-    { timestamps: true }
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		formation: {
+			type: Schema.Types.ObjectId,
+			ref: 'Formation',
+            required: true
+		},
+		playImage: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
 );
 
 export default mongoose.model('Play', playSchema);
--- src/database/services/plays.service.js
+++ src/database/services/plays.service.js
@@ -1,5 +1,35 @@
-import Plays from '../models/Play.model'
+import Play from '../models/Play.model.js';
 
-const PlayClass = {};
+const PlayClass = {
+	createPlay: async newPlay => {
+		const response = await Play.create(newPlay);
+
+		return response;
+	},
+
+	updatePlay: async (playId, updatedInfo) => {
+		const response = await Play.findByIdAndUpdate(playId, updatedInfo);
+
+		return response;
+	},
+
+	getOnePlay: async playId => {
+		const response = await Play.findById(playId);
+
+		return response;
+	},
+
+	deleteOnePlay: async playId => {
+		const response = await Play.findByIdAndDelete(playId);
+
+		return response;
+	},
+
+	deleteManyPlays: async formationId => {
+		const response = await Play.deleteMany({ formation: formationId });
+
+		return response;
+	},
+};
 
 export default PlayClass;
